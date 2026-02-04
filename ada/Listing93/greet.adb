3b0b0967d361a0e931c70360701085938f52d76e
Robertf2003 <robertjr.legend.electric@gmail.com>
2026-02-04 13:36:17 -0600

    WIP

---
+++ ada/Listing93/greet.adb
@@ -0,0 +1,13 @@
+with Ada.Text_IO; use Ada.Text_IO;
+
+procedure Greet is
+    Message : String (1 .. 11) := "dlroW olleH";
+    -- ^ Pre-defined array type.
+    -- Component type is Character
+begin
+    for I in reverse Message'Range loop
+        -- ^ Iterate in reverse order
+        Put (Message (I));
+    end loop;
+    New_Line;
+end Greet;
---
+++ ada/Listing95/main.adb
@@ -0,0 +1,16 @@
+with Ada.Text_IO; use Ada.Text_IO;
+
+procedure Main is
+    type Integer_Array is
+        array (Natural range <>) of Integer;
+
+        My_Array : constant Integer_Array :=
+            (1, 2, 3, 4);
+        -- ^^^^^^^^^^^^^^^^^^^^^
+        -- Bounds are automatically
+        -- computed from
+        -- initialization value
+begin
+    null;
+end Main;
+
